a4c3261ad3552dafaa97c472cc5388038e03dab9
Bryan Head <bryan.head@gmail.com>
2009-06-20 23:38:24 -0700

    Added license stuff; decided to go with the MIT license.

--- src/org/headb/SandpileGraph.java
+++ src/org/headb/SandpileGraph.java
@@ -1,3 +1,32 @@
+/*
+Copyright (c) 2008-2009 Bryan Head
+All Rights Reserved
+
+[This software is released under the "MIT License"]
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the
+Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall
+be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 package org.headb;
 
 //
@@ -396,6 +425,16 @@ public class SandpileGraph {
 	}
 	 */
 
+	/**
+	 * Calculates the identity configuration of the graph.
+	 * WARNING: For big graphs, this can take a long time.
+	 * Currently, the function adds the burning configuration
+	 * to itself until it gets the same thing back. This relies
+	 * on the fact that stabilizing the burning config is linear.
+	 * SECOND WARNING: If the graph does not have an identity (there is no global
+	 * sink or something), this function will never end!
+	 * @return The identity configuration.
+	 */
 	public SandpileConfiguration getIdentityConfig() {
 		SandpileConfiguration burning = getMinimalBurningConfig();
 		LinkedHashSet<Integer> burningVertices = new LinkedHashSet<Integer>();
